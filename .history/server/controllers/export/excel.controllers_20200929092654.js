8bfbf3b55138162b93655206970caf219ebdfa93
Jose Luis <jls0107@gmail.com>
2020-09-29 11:37:52 -0300

    exporter y fix login

---
+++ .history/server/controllers/export/excel.controllers_20200929092654.js
@@ -0,0 +1,42 @@
+// Require library
+var xl = require("excel4node");
+var wb = new xl.Workbook();
+var ws = wb.addWorksheet("Hoja 1");
+const { response } = require("express");
+const { Schema } = require("mongoose");
+const Fc0800 = require("../../models/formcovid/fc0800");
+
+const getFormXls = (req, res = response) => {
+  const datos = req.params.table;
+  Fc0800.find({}, (error, dato) => {
+    if (error) throw error;
+    ws.cell(1, 1).string("Fecha de Registro");
+    ws.cell(1, 2).string("Localidades");
+    ws.cell(1, 3).string("Tipo de Registro");
+    ws.cell(1, 4).string("Realizo hisopado");
+    ws.cell(1, 5).string("Requiere Res. Hisopado");
+    ws.cell(1, 6).string("Número de Registro");
+    ws.cell(1, 7).string("Convivientes");
+    for (let i = 0; i < dato.length; i++) {
+      ws.cell(i + 2, 1)
+        .date(dato[i].fecha)
+        .style({ numberFormat: "dd/MM/yyyy" });
+      ws.cell(i + 2, 2).string(dato[i].persona.localidad);
+      ws.cell(i + 2, 3).string(dato[i].tipo_registro);
+      ws.cell(i + 2, 4).string(dato[i].realizo_hisopado);
+      if (dato[i].resultado_hisopado === true) {
+        ws.cell(i + 2, 5).string("Si");
+      } else {
+        ws.cell(i + 2, 5).string("No");
+      }
+      ws.cell(i + 2, 6).number(dato[i].nroForm);
+      if (dato[i].vivienda_personas === null) {
+        ws.cell(i + 2, 7).number(0);
+      } else {
+        ws.cell(i + 2, 7).number(dato[i].vivienda_personas);
+      }
+    }
+    wb.write("prueba.xlsx", res);
+  });
+};
+module.exports = { getFormXls };
---
+++ .history/server/routes/login.routes_20200919221936.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Usuario = require("../models/usuario");
+const app = express();
+
+app.post("/", async (req, res) => {
+  let body = req.body;
+
+  await Usuario.findOne({ documento: body.documento }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al buscar usuario",
+        errors: err,
+      });
+    }
+
+    if (!usuarioDB) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "Credenciales incorrectas - documento",
+        errors: err,
+      });
+    }
+    const isMatch = bcrypt.compareSync(body.password, usuarioDB.password);
+    if (isMatch) {
+      usuarioDB.lastLogin = Date.now();
+      Usuario.save(usuarioDB.lastLogin);
+      res.status(200).json({
+        ok: true,
+        usuario: usuarioDB,
+        token: token,
+        id: usuarioDB._id,
+      });
+    }
+    return res.status(400).json({
+      ok: false,
+      mensaje: "Credenciales incorrectas - password",
+      errors: err,
+    });
+  });
+});
+
+module.exports = app;
